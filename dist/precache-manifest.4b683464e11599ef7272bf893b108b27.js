616f16544b087f124978a3b8572f812f19b6c28f
Hhpon <624791164@qq.com>
2020-09-27 23:02:59 +0800

    add public

---
+++ dist/precache-manifest.4b683464e11599ef7272bf893b108b27.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6b67672f78d3025bf41",
+    "url": "css/app.5f89f953.css"
+  },
+  {
+    "revision": "c6b67672f78d3025bf41",
+    "url": "js/app.6cb68057.js"
+  },
+  {
+    "revision": "fab6af480aa6aba40786",
+    "url": "css/chunk-130bafb4.87f6b6a3.css"
+  },
+  {
+    "revision": "fab6af480aa6aba40786",
+    "url": "js/chunk-130bafb4.064c562d.js"
+  },
+  {
+    "revision": "03572761961973599f21",
+    "url": "css/chunk-296b8093.5f4e1e86.css"
+  },
+  {
+    "revision": "03572761961973599f21",
+    "url": "js/chunk-296b8093.40980503.js"
+  },
+  {
+    "revision": "77ecebbe4179a0b5e7af",
+    "url": "css/chunk-55198bd8.e9fbea8e.css"
+  },
+  {
+    "revision": "77ecebbe4179a0b5e7af",
+    "url": "js/chunk-55198bd8.fe9596b2.js"
+  },
+  {
+    "revision": "77d431603597a953021f",
+    "url": "js/chunk-59a4c53e.5a809fa7.js"
+  },
+  {
+    "revision": "9f531d0009ee4cc1f0e6",
+    "url": "css/chunk-59d5deff.4e7308e7.css"
+  },
+  {
+    "revision": "9f531d0009ee4cc1f0e6",
+    "url": "js/chunk-59d5deff.f7bea40d.js"
+  },
+  {
+    "revision": "dd8cc232022f756cbb93",
+    "url": "css/chunk-6232c8f0.72d21221.css"
+  },
+  {
+    "revision": "dd8cc232022f756cbb93",
+    "url": "js/chunk-6232c8f0.6ccedbe8.js"
+  },
+  {
+    "revision": "600c847df42b3060dcc3",
+    "url": "css/chunk-a08a1c96.cb251393.css"
+  },
+  {
+    "revision": "600c847df42b3060dcc3",
+    "url": "js/chunk-a08a1c96.380c8d40.js"
+  },
+  {
+    "revision": "c2b09a28b493e55a0efb",
+    "url": "css/chunk-c2c928ec.42c7255c.css"
+  },
+  {
+    "revision": "c2b09a28b493e55a0efb",
+    "url": "js/chunk-c2c928ec.ef13ef89.js"
+  },
+  {
+    "revision": "fecb109906a2747c68e2",
+    "url": "css/chunk-vendors.f10a864c.css"
+  },
+  {
+    "revision": "fecb109906a2747c68e2",
+    "url": "js/chunk-vendors.59b29436.js"
+  },
+  {
+    "revision": "c60c4d39f83feebe9d441d487e67336e",
+    "url": "fonts/cubeic.c60c4d39.woff"
+  },
+  {
+    "revision": "dcc2b6f1e42abe71bc9f1a83f85f47d4",
+    "url": "fonts/cubeic.dcc2b6f1.ttf"
+  },
+  {
+    "revision": "71fa446f385a7357a5ddb0caa6e0a063",
+    "url": "index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+];
